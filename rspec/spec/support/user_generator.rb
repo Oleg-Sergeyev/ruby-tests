1ff7df03c8af52c5e25d2821b860cf62dccbc60e
oleg <sergeyev_ov@mail.ru>
2022-05-18 03:38:05 +0300

    Работа над ошибками урок 4, тесты на User, вызов константы из FFaker

--- rspec/spec/support/user_generator.rb
+++ rspec/spec/support/user_generator.rb
@@ -5,21 +5,17 @@ require 'ffaker'
 # module UserGenerator
 module UserGenerator
   def generate(national, error = nil)
-    user = national == :ru ? cirilyc_names : latin_names
-    user.update({ email: FFaker::Internet.safe_email })
+    user = national == :ru ? names(:NameRU) : names(:Name)
     user.update({ error.keys.first => FFaker::Time.date.to_s }) if error && error[error.keys.first] == :wrong
     [user, national]
   end
 
-  def cirilyc_names
-    { first_name: FFaker::NameRU.first_name,
-      last_name: FFaker::NameRU.last_name,
-      middle_name: FFaker::NameRU.send("middle_name_#{%w[male female].sample}") }
-  end
-
-  def latin_names
-    { first_name: FFaker::Name.first_name,
-      last_name: FFaker::Name.last_name,
-      middle_name: nil }
+  def names(national)
+    hash = { first_name: FFaker.const_get(national).first_name,
+             last_name: FFaker.const_get(national).last_name,
+             middle_name: nil,
+             email: FFaker::Internet.safe_email }
+    hash.update({ middle_name: FFaker::NameRU.send("middle_name_#{%w[male female].sample}") }) if national == :NameRU
+    hash
   end
 end
