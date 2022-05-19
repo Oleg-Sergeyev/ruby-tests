6394f983688b5dd363b152f52c99ebfc90389fdb
oleg <sergeyev_ov@mail.ru>
2022-05-19 19:28:49 +0300

    Поправлен тест, на ковертор чила в размер мегабайты, гигабайты и т.д.

--- rspec/spec/integer_spec.rb
+++ rspec/spec/integer_spec.rb
@@ -1,13 +1,11 @@
 # frozen_string_literal: true
 
-#require_relative '../../lib/services/integer_converter'
-
-RSpec.describe "Сервис объект #{Services::IntegerConverter}" do
+RSpec.describe "Сервис объект #{Services::IntegerConverter}", :lesson do
   let(:test_value) { convert_by_integer_converter(5_000_000) }
   let(:correct_value) { convert_by_tester(5_000_000) }
 
   it 'выводит в результате hash вида {kb: xxx.xx}' do
-    expect(generate).to respond_to :keys
+    expect(test_value).to respond_to :keys
   end
   it 'конвертация провелась корректно' do
     expect(test_value).to eq correct_value
@@ -15,7 +13,7 @@ RSpec.describe "Сервис объект #{Services::IntegerConverter}" do
   it 'конвертирует заданное число в мегабайты' do
     expect(test_value[:mb]).to eq correct_value[:mb]
   end
-  xit 'конвертирует заданное число в байты' do
+  it 'конвертирует заданное число в байты' do
     expect(test_value[:bytes]).to eq correct_value[:bytes]
   end
 end
